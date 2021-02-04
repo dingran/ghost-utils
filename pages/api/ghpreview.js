0bedd6906be24529d7b90f8354b691672a4ebacf
Ran Ding <dingran.nju@gmail.com>
2021-02-04 09:06:31 -0500

    add timing stats

--- pages/api/ghpreview.js
+++ pages/api/ghpreview.js
@@ -8,7 +8,9 @@ export default async (req, res) => {
   const slug = req.query.slug;
 
   if (siteId && slug) {
+    const startTime = new Date();
     const { site } = await dbAdmin.getSite(siteId);
+    console.log('Get site', new Date().getTime() - startTime.getTime(), 'ms');
     console.log(site);
 
     const api = new GhostAdminAPI({
@@ -17,11 +19,20 @@ export default async (req, res) => {
       version: 'v3',
     });
 
+    console.log('Init api', new Date().getTime() - startTime.getTime(), 'ms');
+
     const length = site.truncation_length || 500;
 
     try {
       const response = await api.posts.read({ slug: slug, formats: 'html' });
+      console.log(
+        'Read post',
+        new Date().getTime() - startTime.getTime(),
+        'ms'
+      );
+
       response.html = truncate(response.html, length);
+      console.log('Truncate', new Date().getTime() - startTime.getTime(), 'ms');
       res.statusCode = 200;
       res.json({ response });
     } catch (error) {
