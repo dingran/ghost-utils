e5f084c48cd348bef456986fcc8f537fd8c55519
Ran Ding <dingran.nju@gmail.com>
2021-02-04 08:08:49 -0500

    added firebase db and public api /ghpreview

--- pages/api/ghpreview.js
+++ pages/api/ghpreview.js
@@ -1,17 +1,24 @@
+// this is the public version of ghost.js
 const GhostAdminAPI = require('@tryghost/admin-api');
 const truncate = require('truncate-html');
-const TRUNCATION_LENGTH = 500;
-
-const api = new GhostAdminAPI({
-  url: process.env.GHOST_URL,
-  key: process.env.GHOST_ADMIN_KEY,
-  version: 'v3',
-});
+const dbAdmin = require('@/lib/db-admin');
 
 export default async (req, res) => {
+  const siteId = req.query.siteId;
   const slug = req.query.slug;
-  if (slug) {
-    const length = TRUNCATION_LENGTH;
+
+  if (siteId && slug) {
+    const { site } = await dbAdmin.getSite(siteId);
+    console.log(site);
+
+    const api = new GhostAdminAPI({
+      url: site.api_url,
+      key: site.admin_api_key,
+      version: 'v3',
+    });
+
+    const length = site.truncation_length || 500;
+
     try {
       const response = await api.posts.read({ slug: slug, formats: 'html' });
       response.html = truncate(response.html, length);
@@ -24,6 +31,8 @@ export default async (req, res) => {
     }
   } else {
     res.statusCode = 400;
-    res.json({ error: 'must provide slug in request query parameter' });
+    res.json({
+      error: 'must provide slug AND siteId in request query parameter',
+    });
   }
 };
---
+++ public/static/ghpreview.js
@@ -0,0 +1,48 @@
+(function () {
+  var ghpreviewScript = document.currentScript;
+  var siteId = ghpreviewScript.getAttribute('data-site');
+
+  // strip trailing slash then split by slash
+  var pathArray = window.location.pathname.replace(/\/$/, '').split('/');
+  var slug = pathArray[pathArray.length - 1];
+
+  // var domainUrl = 'http://localhost:3000'; // local dev
+  var domainUrl = 'https://utils.dingran.me';
+  var previewEndpoint = `${domainUrl}/api/ghpreview?siteId=${siteId}&slug=${slug}`;
+
+  var style = document.createElement('style'),
+    css = `
+  .ghpreview-membersonly-excerpt {
+    position: relative;
+  }
+  
+  .ghpreview-membersonly-excerpt:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 70%;
+    background: linear-gradient(
+      to top,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0.9) 30%,
+      rgba(255, 255, 255, 0) 100%
+    );
+  }`,
+    head = document.head || document.getElementsByTagName('head')[0];
+  style.appendChild(document.createTextNode(css));
+  head.appendChild(style);
+
+  console.log(previewEndpoint);
+  fetch(previewEndpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      const div = document.createElement('div');
+      div.innerHTML = data.response.html;
+      div.className = 'ghpreview-membersonly-excerpt';
+      document
+        .querySelector('aside[class*="upgrade-cta"]')
+        .parentNode.prepend(div);
+    });
+})();
