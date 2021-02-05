e6c867a19464b2efe573eeefb058a0a970d95fdc
Ran Ding <dingran.nju@gmail.com>
2021-02-05 06:50:57 -0500

    added user auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+import { AuthProvider } from '../lib/auth';
+const App = ({ Component, pageProps }) => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+};
+export default App;
