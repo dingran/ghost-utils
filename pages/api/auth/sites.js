c2abaa5cc032737ab35c8cf8b1cc0554a9630920
Ran Ding <dingran.nju@gmail.com>
2021-02-09 08:42:37 -0500

    added withAuth to sites route

--- pages/api/auth/sites.js
+++ pages/api/auth/sites.js
@@ -1,24 +1,14 @@
-import { auth } from '@/lib/firebase-admin';
+import { withAuth } from '@/lib/middlewares';
 import { getUserSites } from '@/lib/db-admin';
 
-export default async (req, res) => {
-  try {
-    const { uid } = await auth.verifyIdToken(req.headers.token);
-    req.uid = uid;
-  } catch (error) {
-    // token issues
-    // console.log(error);
-    const msg = { ...error.errorInfo };
-    // console.log(msg);
-    res.status(400).json(msg);
-    return;
-  }
-
+const handler = async (req, res) => {
   try {
     const { sites } = await getUserSites(req.uid);
-    res.status(200).json({ sites });
+    return res.status(200).json({ sites });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
+
+export default withAuth(handler);
