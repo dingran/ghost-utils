cb9ce569f1920134177d77ec3a9576faa85126ee
Ran Ding <dingran.nju@gmail.com>
2021-02-19 06:51:31 -0500

    add plausible.io analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,12 @@ class MyDocument extends Document {
             rel='stylesheet'
           />
           <script
+            async
+            defer
+            data-domain='ghutils.dingran.me'
+            src='https://plausible.io/js/plausible.js'
+          ></script>
+          <script
             type='text/javascript'
             dangerouslySetInnerHTML={{
               __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="960c9de1-2146-4fb1-8ded-0595aa107bd1";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
