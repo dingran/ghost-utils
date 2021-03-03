0d8c3fc05a825c1203158951ae523bb21a7748bc
Ran Ding <dingran.nju@gmail.com>
2021-03-03 07:39:12 -0500

    clean up

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Head from 'next/head';
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import { useAuth } from '@/lib/auth';
-import { Flex, Box } from '@chakra-ui/react';
 import Hero from '@/components/Hero';
 import PageShell from '@/components/PageShell';
 import Showcase from '@/components/Showcase';
 
 export default function Home() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => alert(JSON.stringify(data));
-
   const auth = useAuth();
   return (
     <>
